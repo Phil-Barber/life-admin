4c09e4f07a63fdc8d7cba7a30189a391cc543563
Phil Barber <phil.barber93@gmail.com>
2018-06-28 09:00:29 +0100

    adding TaskForm Button

--- life-admin-app/containers/App.js
+++ life-admin-app/containers/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import { Button } from 'react-native';
 import { 
   StackNavigator
 } from 'react-navigation';
 import TaskFeed from './TaskFeed';
 import TaskFocus from './TaskFocus';
+import TaskForm from './TaskForm';
 
 const RootStack = StackNavigator(
   {
     TaskFeed : { screen: TaskFeed },
-    TaskFocus : { screen: TaskFocus }
+    TaskFocus : { screen: TaskFocus },
+    TaskForm : { screen: TaskForm }
   },
   {
     initialRouteName: 'TaskFeed',
     navigationOptions: {
+      headerRight: <Button title='hello' onPress={() => newTask()} />,
       headerStyle: {
         padding:10
       }
     } 
   }
 );
+
+// TODO How to navigate from here?
+function newTask() {
+    this.props.navigation.navigate('TaskForm');
+  }
 
 export default class App extends React.Component {
   render() {
